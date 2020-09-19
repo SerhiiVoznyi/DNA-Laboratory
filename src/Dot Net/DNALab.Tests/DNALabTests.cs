54e71f8515d35c21f32211ac32f7e6a3cd585a4b
HOME-PC\Serhii <serhiivoznyi@gmail.com>
2020-09-19 20:26:52 +0200

    Added License file, and additional tests

---
+++ src/Dot Net/DNALab.Tests/DNALabTests.cs
@@ -0,0 +1,21 @@
+﻿namespace DNALab.Tests
+{
+    using Shouldly;
+    using Xunit;
+
+    public class DNALabTests
+    {
+        public readonly DNALab _sut;
+
+        public DNALabTests()
+        {
+            _sut = new DNALab();
+        }
+
+        [Fact]
+        public void Text_ShouldBeNull()
+        {
+            _sut.Text.ShouldBeNull();
+        }
+    }
+}
